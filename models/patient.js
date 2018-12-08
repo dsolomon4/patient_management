55c46402566dd2da394d619f98a1b862786595c2
Desiree Solomon <d.d.solomon4@gmail.com>
2018-12-08 10:30:23 -0500

    handlebars3

--- models/patient.js
+++ models/patient.js
@@ -30,7 +30,7 @@ module.exports = function (sequelize, DataTypes) {
         },
         address2: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: true,
             validate: {
                 len: [1]
             }
@@ -57,7 +57,7 @@ module.exports = function (sequelize, DataTypes) {
             }
         },
         phone_number: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
             allowNull: false,
             validate: {
                 len: [10]
--- server.js
+++ server.js
@@ -15,6 +15,13 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
 // Static directory
 app.use(express.static("public"));
 
