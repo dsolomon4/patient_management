631519daca330545374c2b03468e5a46857c86b8
Desiree Solomon <d.d.solomon4@gmail.com>
2018-12-22 15:20:13 -0500

    ds3 update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # patient_management
 
 Patient Managemnt System to keep track of current and new patients for nurses and doctors
+
+## Technologies Used
+
+JavaScript, Node, Express, Javascript, HTML, CSS, Bootstrap, jQuery, MySql, Sequelize, Router, 
--- public/js/view.js
+++ public/js/view.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 
     // })
 
+  
+
     var pId = window.location.href.split("patientview/")[1]
     $.ajax("/api/patient/" + pId,{
         type: "GET",
@@ -10,7 +12,32 @@ $(document).ready(function () {
     }).then(function(data){
         console.log(data)
         $("#patient_name").html("First Name: " + data.first_name + "First Name: " + data.last_name)
+
+
+        completePatient(data)
+
+        function completePatient(data){
+            $("#complete").on("click", function(event){
+                event.preventDefault();
+            
+                $.ajax({
+                    method: 'PUT',
+                    url: '/api/patients/' + data.id,
+                    data: {
+                        active: false
+                    }
+                }).then(result => {
+                    console.log(result)
+        
+                    location.href = "/";
+                })
+        
+            
+            
+            })
+        }
     });
+    
 
     $("#see-doctor").on("click", function(event){
         event.preventDefault();
@@ -35,39 +62,5 @@ $(document).ready(function () {
 
 
 
-    // $("#submit-search").on("click", function (event) {
-    //     event.preventDefault();
-
-
-    //     var searchPatient = {
-    //         first_name: $("#firstName").val(),
-    //         last_name: $("#lastName").val(),
-            
-    //     };
-
-    //     $("#new-patient-form").hide();
-
-    //     console.log(searchPatient.first_name + " has been searched")
-
-
-    //     var currentURL = window.location.origin;
-
-    //     $.get(currentURL + "/api/patients", searchPatient, function (data) {
-
-
-    //         console.log(data.first_name)
-    //         document.getElementById("patient-added").innerText = data.first_name + " " + data.last_name + " has succesfully be added";
-    //         $("#return-home").show();
-
-    //     });
-
-
-
-    // });
-
-
-
-
-
-
 })
+
